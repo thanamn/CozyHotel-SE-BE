edb65b036866a73169b710306411ea1f00ec265a
Kanomtom-aroi <salami.learn@gmail.com>
2025-03-02 00:28:42 +0700

    Add limiter

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser')
-const connectDB = require('./config/db');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const { xss } = require('express-xss-sanitizer');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 // route files
-const hotels = require('./routes/hotels')
-const auth = require('./routes/auth');
-const bookings =require('./routes/bookings');
+const hotels = require("./routes/hotels");
+const auth = require("./routes/auth");
+const bookings = require("./routes/bookings");
 
 // load dotenv
-dotenv.config({path:'./config/config.env'})
+dotenv.config({ path: "./config/config.env" });
+
+// rate limit
+const limiter = rateLimit({
+  windowsMs: 10 * 50 * 1000,
+  max: 100,
+});
 
 // connect DB here
 connectDB();
 
-const app=express();
+const app = express();
 
 // body parser
 app.use(express.json());
@@ -32,10 +38,19 @@ app.use(helmet());
 app.use(xss());
 app.use(hpp());
 app.use(cors());
+app.use(limiter);
 
-app.use('/api/v1/hotels', hotels);
-app.use('/api/v1/bookings', bookings)
-app.use('/api/v1/auth',auth);
+app.use("/api/v1/hotels", hotels);
+app.use("/api/v1/bookings", bookings);
+app.use("/api/v1/auth", auth);
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT))
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    "Server running in ",
+    process.env.NODE_ENV,
+    " mode on port ",
+    PORT
+  )
+);
