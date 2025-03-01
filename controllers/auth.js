3f03b23f273304c39f99a20d1f1bdfcca7bc3561
Kanomtom-aroi <salami.learn@gmail.com>
2025-03-01 21:30:13 +0700

    Add Logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -82,3 +82,17 @@ exports.getMe=async(req, res, next)=>{
         data:user
     });
 }
+
+
+exports.logout= async(req,res,next) => {
+    res.cookie('token','none' ,{
+      expires: new Date(Date.now() + 10*1000),
+      httpOnly:true
+    });
+  
+    res.status(200).json({
+      success:true,
+      data:{}
+    });
+  };
+  
