ec3f0e46a7c88c9c0fa1a07d08d56a0f961ba45d
Ittyy <96482477+Ittyy-Winter@users.noreply.github.com>
2025-03-03 14:49:05 +0700

    Forget Something

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,14 +2,14 @@ const { Router } = require("express");
 const {getHotels, getHotel, createHotel, updateHotel, deleteHotel} =require('../controllers/hotels')
 
 //Include other resource routers
-const appointmentRouter=require('./bookings');
+const bookingRouter=require('./bookings');
 
 const router=Router();
 
 const {protect, authorize} = require('../middleware/auth');
 
 //Re-route into other resource routers
-router.use('/:hotelId/bookings/', appointmentRouter);
+router.use('/:hotelId/bookings/', bookingRouter);
 
 router.route('/').get(getHotels).post(protect, authorize('admin'), createHotel);
 router.route('/:id').get(getHotel).put(protect, authorize('admin'), updateHotel).delete(protect, authorize('admin'), deleteHotel);
